c398500751550b6e077e393ae3250c46430949ce
Kiashen Maharajh <ST10055763@vcconnect.edu.za>
2024-10-04 16:27:12 +0200

    Initial commit

---
+++ app/src/main/java/com/example/mapsapp/MainActivity.kt
@@ -0,0 +1,38 @@
+package com.example.mapsapp
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.mapbox.geojson.Point
+import com.mapbox.maps.CameraOptions
+import com.mapbox.maps.MapView
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var mapView: MapView
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        // Create a map programmatically and set the initial camera
+        mapView = MapView(this)
+        mapView.mapboxMap.setCamera(
+            CameraOptions.Builder()
+                .center(Point.fromLngLat(-98.0, 39.5))
+                .pitch(0.0)
+                .zoom(2.0)
+                .bearing(0.0)
+                .build()
+        )
+        // Add the map view to the activity (you can also add it to other views as a child)
+        setContentView(mapView)
+    }
+}
